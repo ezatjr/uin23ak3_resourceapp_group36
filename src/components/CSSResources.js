66f931442e86ba8f06e318dadeb063d9dbac1c5b
ezatjr <Ezatj@hiof.no>
2024-03-19 00:23:44 +0500

    SAAS la til

--- src/components/CSSResources.js
+++ src/components/CSSResources.js
@@ -2,16 +2,16 @@ import React from 'react'; //Importer react, noe som er nødvendig for å bruker
 import resources from '../resources'; //Henter filen resources.
 
 const HTMLResources = () => {
-  const htmlResources = resources.find(resource => resource.category === 'CSS'); //resources array brukes for å finne ressursen som tilhører kstergorien CSS ved hjelp av .find
+  const Resource = resources.find(resource => resource.category === 'CSS'); //resources array brukes for å finne ressursen som tilhører kstergorien CSS ved hjelp av .find
 
   return (
     <>
-      <h2>{htmlResources.category}</h2> {/* Overskriften viser kategorien til ressursen */}
-      <p>{htmlResources.text}</p> {/* P-tag som viser teksten til ressursen. */}
+      <h2>{Resource.category}</h2> {/* Overskriften viser kategorien til ressursen */}
+      <p>{Resource.text}</p> {/* P-tag som viser teksten til ressursen. */}
 
       <ul>
         {/* Mapper gjennom arrayen. Sources skal inneholde informasjon om ulike kilder til ressurser */}
-        {htmlResources.sources.map((source, index) => ( 
+        {Resource.sources.map((source, index) => ( 
           <li key={index}><a href={source.url}>{source.title}</a></li>
         ))}
         {/* For hver kilde, blir det generert listeelement. Key Index brukes for å gi hvert <li> en unik nøkkel basert på indeksen i arrayen */}
--- src/components/HTMLResources.js
+++ src/components/HTMLResources.js
@@ -2,20 +2,18 @@ import React from 'react'; //Importer react, noe som er nødvendig for å bruker
 import resources from '../resources'; //Henter filen resources.
 
 const HTMLResources = () => {
-  const htmlResources = resources.find(resource => resource.category === 'HTML'); //sammer som CSS men den finner kategorien HTML ved hjelp av find, deretter blir den lagret i html variabelen.
+  const Resource = resources.find(resource => resource.category === 'HTML');
 
   return (
     <>
-      <h2>{htmlResources.category}</h2> 
-      <p>{htmlResources.text}</p>
+      <h2>{Resource.category}</h2>
+      <p>{Resource.text}</p>
       <ul>
-        {htmlResources.sources.map((source, index) => (
-      {htmlResources.sources.map((source, index) => (
+        {Resource.sources.map((source, index) => (
           <li key={index}><a href={source.url}>{source.title}</a></li>
         ))}
       </ul>
     </>
-    </> 
   );
 }
 
