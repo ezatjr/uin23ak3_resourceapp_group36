d727908bde7ea468b832550844dca1bee571a45f
ezatjr <142840867+ezatjr@users.noreply.github.com>
2024-03-18 09:49:11 +0500

    Oppdater App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
     <Router>
       <Routes>
       <Route path="/" element={<Layout><HTMLResources /></Layout>} />
-        <Route path="/html" element={<Layout><HTMLResources /></Layout>} /> {/* Wrap HTMLResources with Layout */}
+        <Route path="/html" element={<Layout><HTMLResources /></Layout>} /> 
         <Route path="/css" element={<Layout><CSSResources /></Layout>} />
         <Route path="/javascript" element={<Layout><JavaScriptResources /></Layout>} />
         <Route path="/react" element={<Layout><ReactResources /></Layout>} />
