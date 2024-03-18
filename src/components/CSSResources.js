56e79cd5e2e69d27e98e49189c73c1c5e000016b
ezatjr <ezatj@hiof.no>
2024-03-18 16:57:09 +0100

    CSS-ressurser
    "La til HTMLResources-komponenten for å vise CSS-ressurser.

--- src/components/CSSResources.js
+++ src/components/CSSResources.js
@@ -2,12 +2,13 @@ import React from 'react';
 import resources from '../resources';
 
 const HTMLResources = () => {
-  const htmlResources = resources.find(resource => resource.category === 'CSS');
+  const htmlResources = resources.find(resource => resource.category === 'CSS'); //resources array brukes for å finne ressursen som tilhører kstergorien CSS ved hjelp av .find
 
   return (
     <>
-      <h2>{htmlResources.category}</h2>
-      <p>{htmlResources.text}</p>
+      <h2>{htmlResources.category}</h2> {/* Overskriften viser kategorien til ressursen */}
+      <p>{htmlResources.text}</p> {/* P-tag som viser teksten til ressursen. */}
+
       <ul>
         {htmlResources.sources.map((source, index) => (
           <li key={index}><a href={source.url}>{source.title}</a></li>
@@ -16,5 +17,6 @@ const HTMLResources = () => {
     </>
   );
 }
+
 
 export default HTMLResources;
