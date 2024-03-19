2bf582045ecf1e003de8bb1c96cd7905233305ef
ezatjr <Ezatj@hiof.no>
2024-03-19 23:44:49 +0500

    Styling lagt til

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Layout.scss';
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+
   return (
     <div className="container">
       <div className="tab-container">
         <ul className="tab-menu">
-          <li><Link to="/html">HTML</Link></li>
-          <li><Link to="/css">CSS</Link></li>
-          <li><Link to="/javascript">JavaScript</Link></li>
-          <li><Link to="/react">React</Link></li>
-          <li><Link to="/sanity">Sanity and headless CMS</Link></li>
+          <li className={location.pathname === "/html" ? "active" : ""}><Link to="/html">HTML</Link></li>
+          <li className={location.pathname === "/css" ? "active" : ""}><Link to="/css">CSS</Link></li>
+          <li className={location.pathname === "/javascript" ? "active" : ""}><Link to="/javascript">JavaScript</Link></li>
+          <li className={location.pathname === "/react" ? "active" : ""}><Link to="/react">React</Link></li>
+          <li className={location.pathname === "/sanity" ? "active" : ""}><Link to="/sanity">Sanity and headless CMS</Link></li>
         </ul>
       </div>
       <main>
         <div className="resource-details">
-          {children} {/* Gjengi barnekomponenter her */}
+          {children}
         </div>
       </main>
     </div>
