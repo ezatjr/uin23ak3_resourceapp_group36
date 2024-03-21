d30178ecb964687427682252af279d21452ed2d3
eztajr <Ezatj@hiof.no>
2024-03-21 12:16:59 +0500

    resources.js Oppdatert og skjermrespons

--- src/components/CSSResources.js
+++ src/components/CSSResources.js
@@ -2,20 +2,16 @@ import React from 'react'; //Importer react, noe som er nødvendig for å bruker
 import resources from '../resources'; //Henter filen resources.
 
 const HTMLResources = () => {
-  const Resource = resources.find(resource => resource.category === 'CSS'); //resources array brukes for å finne ressursen som tilhører kstergorien CSS ved hjelp av .find
-
+  const cssResources = resources.filter(resource => resource.category === 'css');
   return (
     <>
-      <h2>{Resource.category}</h2> {/* Overskriften viser kategorien til ressursen */}
-      <p>{Resource.text}</p> {/* P-tag som viser teksten til ressursen. */}
-
+      <h2>CSS Resources</h2>
       <ul>
-        {/* Mapper gjennom arrayen. Sources skal inneholde informasjon om ulike kilder til ressurser */}
-        {Resource.sources.map((source, index) => ( 
-          <li key={index}><a href={source.url}>{source.title}</a></li>
+        {cssResources.map((resource, index) => (
+          <li key={index}>
+            <a href={resource.url}>{resource.title}</a>
+          </li>
         ))}
-        {/* For hver kilde, blir det generert listeelement. Key Index brukes for å gi hvert <li> en unik nøkkel basert på indeksen i arrayen */}
-        {/* Inni hvert <li> er det også lagd en a href som er URL til kilden. */}
       </ul>
     </>
   );
